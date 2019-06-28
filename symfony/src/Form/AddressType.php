6a932e3994913a3efbb7fd36bd4b0f2f609ca5ca
joelSYLVIUS <joel.sylvius@hotmail.fr>
2019-06-28 16:46:28 +0200

    add crud bill, artistypes, address with design

---
+++ symfony/src/Form/AddressType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('street')
+            ->add('street_line2')
+            ->add('city')
+            ->add('zip_code')
+            ->add('country')
+            ->add('id_user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
