180e01ca8ad40d7a92b6df0e26d1a8aaf85237d1
Virgil <virgilmeaup@hotmail.fr>
2019-07-02 18:08:01 +0200

    edit profile form

--- symfony/src/Form/UserType.php
+++ symfony/src/Form/UserType.php
@@ -11,6 +11,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -28,7 +29,7 @@ class UserType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'label.email',
-            ]);
+            ])
         ;
     }
     /**
